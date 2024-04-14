3ba1045dc88e750e87bd1d619108a3988f6e3fa0
shubham4132 <shubhamg3122134@gmail.com>
2024-04-14 17:42:50 +0530

    working on to display updated balance on the screen using redux

--- src/features/accounts/BalanceDisplay.js
+++ src/features/accounts/BalanceDisplay.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
 function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
     style: "currency",
@@ -6,7 +9,31 @@ function formatCurrency(value) {
 }
 
 function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+  const balance = useSelector((store) => store.account.balance);
+  //const dispatch = useDispatch();
+
+  return <div className="balance">{formatCurrency(balance)}</div>;
 }
 
 export default BalanceDisplay;
+
+//OLD WAYS TO WRITE CODE
+/*import { connect } from "react-redux";
+function formatCurrency(value) {
+  return new Intl.NumberFormat("en", {
+    style: "currency",
+    currency: "USD",
+  }).format(value);
+}
+
+function BalanceDisplay({ balance }) {
+  return <div className="balance">{formatCurrency(balance)}</div>;
+}
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  };
+}
+
+export default connect(mapStateToProps)(BalanceDisplay);
+*/
