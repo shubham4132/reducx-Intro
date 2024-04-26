3d340813355e2b6667fd7ab94ec0e7c59a44cf21
shubham4132 <shubhamg3122134@gmail.com>
2024-04-26 15:39:30 +0530

    creating the customer Slice

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,35 +1,33 @@
-const initialStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
   fullName: "",
   nationalID: "",
   createdAt: "",
 };
-export default function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload,
-        nationalID: action.payload.nationalID,
-        createdAt: action.payload.createdAt,
-      };
-    case "customer/updateName":
-      return { ...state, fullName: action.payload };
-    default:
-      return state;
-  }
-}
-export function createCustomer(fullName, nationalID) {
-  return {
-    type: "customer/createCustomer",
-    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
-  };
-}
-export function updateName(fullName) {
-  return { type: "customer/updateName", payload: fullName };
-}
-/*
-store.dispatch(createCustomer("shubham goswami", "254848"));
-console.log(store.getState());
-store.dispatch(deposit(250));
-console.log(store.getState());
-*/
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducer: {
+    createCustomer: {
+      prepare: function (fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createSlice;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+export const { createCustomer, updateName } = customerSlice.action;
+export default customerSlice.reducer;
